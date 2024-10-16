ad7e39bff0f30fec4a0ca3aa37f25e566ad89b88
K-Siim <kevinsiim007@gmail.com>
2024-10-16 16:42:26 +0300

    yl13

---
+++ yl13.py
@@ -0,0 +1,9 @@
+lemmikloom = str(input("Mis on sinu lemmikloom?"))
+first_letter = lemmikloom[0]
+print(first_letter)
+list = ["Kass", "Hamster", "Madu"]
+print(list)
+list.append(lemmikloom) 
+print(list)
+first_letter = lemmikloom[-1]
+print(first_letter)
